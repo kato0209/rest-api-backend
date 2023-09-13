c1bc0b28c4445f5a922aadf2f6feb1ead76bb427
Your Name <kato.921002@gmail.com>
2023-09-13 21:36:38 +0900

    first

---
+++ migrate/migrate.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"echo-REST-API/db"
+	"echo-REST-API/model"
+	"fmt"
+)
+
+func main() {
+	dbConn := db.NewDB()
+	defer fmt.Println("Successfully Migrated")
+	defer db.CloseDB(dbConn)
+	dbConn.AutoMigrate(&model.User{}, &model.Task{})
+}
